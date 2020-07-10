55d7c3b95bc9878573765825f48c8a00b4af61a6
Nabbie <nabbielynx@gmail.com>
2020-07-10 13:00:23 -0500

    Update App.js

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  state  = {
+    hello: null
+  }
+  // this is an API request to get the data from the express server
+  componentDidMount () {
+    axios.get('/hello')
+    .then(res => this.setState({hello: res.data}))
+    .catch(err => console.log(err))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div>
+  {this.state.hello
+    ? <div> {this.state.hello} <div>
+    : null }
+</div>
   );
 }
 
